2b00e5324aead535d32985d82be0aa8992536092
Luiz Affonso <luizbuchhalter@outlook.com>
2021-05-02 21:14:42 -0300

    Define deploy method for SSH

--- 1_single_responsibility.rb
+++ 1_single_responsibility.rb
@@ -10,4 +10,14 @@ class Server
       ssh.execute "git clone git@github.com:username/repo.git"
     end
   end
+
+  def deploy
+    start_ssh self, as: "root" do |ssh|
+      ssh.execute "cd repo"
+      ssh.execute "git remote update"
+      ssh.execute "touch tmp/restart.txt"
+    end
+  end
+
+  
 end
