e3e1fa11dc067d9a54bfd06e5de488e946cbde10
Luiz Affonso <luizbuchhalter@outlook.com>
2021-05-02 21:13:09 -0300

    Define setup method for SSH

--- 1_single_responsibility.rb
+++ 1_single_responsibility.rb
@@ -1,3 +1,13 @@
 class Server < ApplicationRecord
   has_many :apps
+  validates :name, presence: true
+end
+
+class Server
+  def setup
+    start_ssh self, as: "root" do |ssh|
+      ssh.execute "apt install ruby"
+      ssh.execute "git clone git@github.com:username/repo.git"
+    end
+  end
 end
