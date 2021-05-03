6dfcb49043bc91c975eb3fb72edef74bd614b68c
Luiz Affonso <luizbuchhalter@outlook.com>
2021-05-02 21:26:37 -0300

    Separate with Single Responsibility

--- 1_single_responsibility.rb
+++ 1_single_responsibility.rb
@@ -1,24 +1,31 @@
+# Every module or class should have responsibility over a single part of functionality
+# and that responsibility should be entirely encapsulated
+# Hatchbox
 class Server < ApplicationRecord
   has_many :apps
   validates :name, presence: true
 end
 
-class Server
-  def setup
+class Server::Management
+  def perform
     start_ssh self, as: "root" do |ssh|
       ssh.execute "apt install ruby"
       ssh.execute "git clone git@github.com:username/repo.git"
     end
   end
+end
 
-  def deploy
+class Server::Deploy
+  def perform
     start_ssh self, as: "root" do |ssh|
       ssh.execute "cd repo"
       ssh.execute "git remote update"
       ssh.execute "touch tmp/restart.txt"
     end
   end
+end
 
+class Server::SSH
   def start_ssh as: "deploy", &block
     Net::SSH.start self.ip do |ssh|
       block.call ssh
