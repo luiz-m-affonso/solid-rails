9bb08529173ead5d36624a2b853c7c2903e4d836
Luiz Affonso <luizbuchhalter@outlook.com>
2021-07-24 20:50:52 -0300

    Apply no-single-responsibility

--- 1. single_responsibility/single_responsibility.rb
+++ 1. single_responsibility/single_responsibility.rb
@@ -6,19 +6,20 @@
 class Server < ApplicationRecord
   has_many :appos
   validates_presence_of :name
-end
 
-def setup
-  start_ssh self, as: "root" do |ssh|
-    ssh.execute 'apt install ruby'
-    ssh.execute 'git clone git@github.com:username/repo.git'
+
+  def setup
+    start_ssh self, as: "root" do |ssh|
+      ssh.execute 'apt install ruby'
+      ssh.execute 'git clone git@github.com:username/repo.git'
+    end
   end
-end
 
-def deploy
-  start_ssh self, as: "root" do |ssh|
-    ssh.execute 'cd repo'
-    ssh.execute 'git remote update'
-    ssh.execute 'touch tmp/restart.txt'
+  def deploy
+    start_ssh self, as: "root" do |ssh|
+      ssh.execute 'cd repo'
+      ssh.execute 'git remote update'
+      ssh.execute 'touch tmp/restart.txt'
+    end
   end
 end
