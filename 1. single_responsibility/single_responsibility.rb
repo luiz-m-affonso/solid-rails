f4a001cfc35c55e084e8172477d1b444ece37037
Luiz Affonso <luizbuchhalter@outlook.com>
2021-07-24 20:48:19 -0300

    New exercises

---
+++ 1. single_responsibility/single_responsibility.rb
@@ -0,0 +1,16 @@
+# Every module or class should have responsibility over a single part of functionality
+# and that responsibility should be entirely encapsulated
+
+# Hatchbox.io
+
+class Server < ApplicationRecord
+  has_many :appos
+  validates_presence_of :name
+end
+
+def setup
+  start_ssh self, as: "root" do |ssh|
+    ssh.execute 'apt install ruby'
+    ssh.execute 'git clone git@github.com:username/repo.git'
+  end
+end
