b5e39690fbd0ae6f3d2fe33728b38d1e7cec4ab9
Luiz Affonso <luizbuchhalter@outlook.com>
2021-07-25 00:37:33 -0300

    Change folders names

--- 1. single_responsibility/app/scripts/servers/create_database.rb
+++
@@ -1,3 +0,0 @@
-class Server::CreateDatabase < Server::SSH
-  def perform(name, username, password); end
-end
--- 1. single_responsibility/app/scripts/servers/ssh.rb
+++
@@ -1,9 +0,0 @@
-class Server::SSH
-  def start_ssh as: 'deploy', &block
-    Net::SSH.start self.ip do |ssh|
-      block.call ssh
-    end
-  end
-
-  def logger; end
-end
